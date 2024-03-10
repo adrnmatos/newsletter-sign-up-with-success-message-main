b4beaadd5879728ec7e71777c1ecc7d28bee00d1
Adriano Matos <adrnmatos@gmail.com>
2024-03-09 21:59:52 -0400

    mobile design & active states

--- script.js
+++ script.js
@@ -1,9 +1,24 @@
-function formsubmit() {
-	// obtem os panels
-	let signUpPanel = document.getElementById('sign-up');
-	let successPanel = document.getElementById('success');
+function formSubmit(event) {
+	alert('chegou');
+	let email = document.getElementById('email');
+	if (!email.value.includes('@')) failure();
+	else success();
 
-	// toogle signUpPanel and successPanel display
-	signUpPanel.classList.toggle('hidden');
-	successPanel.classList.toggle('hidden');
+	event.preventDefault();
 }
+
+function failure() {
+	let errorMessage = document.getElementById('error-label');
+	errorMessage.classList.toggle('hidden');
+
+	let inputElement = document.getElementById('email');
+	inputElement.classList.toggle('input-error');
+}
+
+function success() {
+	let signUpPanel = document.getElementById('sign-up-pane');
+	signUpPanel.classList.toggle('hidden');
+}
+
+let form = document.getElementById('form');
+form.addEventListener('submit', formSubmit);
